3f17f3587d4f5d2438534e81e2fb4de4f5d129a4
Fabio Cuomo <fabio.cuomo@intros.at>
2022-01-03 15:37:45 +0100

    fix: tests the variable before to process

--- step.sh
+++ step.sh
@@ -3,9 +3,11 @@ set -ex
 
 curl -Ls https://install.tuist.io | bash
 
-IFS=' ' read -ra ADDR <<< "$env_vars"
-for i in "${ADDR[@]}"; do
-  export $i
-done
+if [ "$env_vars" != "" ]; then
+    IFS=' ' read -ra ADDR <<< "$env_vars"
+    for i in "${ADDR[@]}"; do
+    export $i
+    done
+fi
 
 cd $BITRISE_SOURCE_DIR/GripApp && tuist $command $arguments $options
