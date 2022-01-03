eaa55da97386b93cae69d7633e9368a2af34d70f
Fabio Cuomo <fabio.cuomo@intros.at>
2022-01-03 15:05:57 +0100

    chore: get env vars and export them

--- step.sh
+++ step.sh
@@ -3,4 +3,9 @@ set -ex
 
 curl -Ls https://install.tuist.io | bash
 
-cd $BITRISE_SOURCE_DIR/GripApp && $env_vars tuist $command $arguments $options
+IFS=' ' read -ra ADDR <<< "$$env_vars"
+for i in "${ADDR[@]}"; do
+  export $i
+done
+
+cd $BITRISE_SOURCE_DIR/GripApp && tuist $command $arguments $options
