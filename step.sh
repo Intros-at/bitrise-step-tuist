b82c690bbe889cfb838806bb9aa3fef4057ba3ac
Fabio Cuomo <fabio.cuomo@intros.at>
2022-01-03 12:19:04 +0100

    fix: adds main root repo to change dir

--- step.sh
+++ step.sh
@@ -3,4 +3,4 @@ set -ex
 
 curl -Ls https://install.tuist.io | bash
 
-cd $BITRISE_SOURCE_DIR && $env_vars tuist $command $arguments $options
+cd $BITRISE_SOURCE_DIR/GripApp && $env_vars tuist $command $arguments $options
