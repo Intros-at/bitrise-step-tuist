01df741bde54a5f86ee86d40e48a06de21730d83
Fabio Cuomo <fabio.cuomo@intros.at>
2022-01-05 15:01:57 +0100

    fix: wraps arguments and options in quotes

--- step.sh
+++ step.sh
@@ -3,4 +3,4 @@ set -ex
 
 curl -Ls https://install.tuist.io | bash
 
-cd $BITRISE_SOURCE_DIR/GripApp && tuist $command $arguments $options
+cd $BITRISE_SOURCE_DIR/GripApp && tuist $command "${$arguments}" "${$options}"
