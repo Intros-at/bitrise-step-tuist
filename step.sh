bf337ab9c5e8481d1b33182ad07dc0c62cdc183e
Fabio Cuomo <fabio.cuomo@intros.at>
2022-01-05 10:10:40 +0100

    fix: removes not used env vars

--- step.sh
+++ step.sh
@@ -3,11 +3,4 @@ set -ex
 
 curl -Ls https://install.tuist.io | bash
 
-if [ "$env_vars" != "" ]; then
-    IFS=' ' read -ra ADDR <<< "$env_vars"
-    for i in "${ADDR[@]}"; do
-    export $i
-    done
-fi
-
 cd $BITRISE_SOURCE_DIR/GripApp && tuist $command $arguments $options
